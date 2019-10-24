52ec89357a64a63174692c1f3886e06f18bbd7e0
Vlad Pavel <vlad.pavel@autodesk.com>
2019-10-24 09:16:48 +0300

    remove unused CustomAction

--- src/CustomAction/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("CustomAction")]
-[assembly: AssemblyCulture("")]
-[assembly: Guid("dc4cac49-9cd1-4454-8215-39cc197caf3f")]
