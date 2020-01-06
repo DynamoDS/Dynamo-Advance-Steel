0a115616e2368ffb2770266951132351b6cbbaf8
Vlad Pavel <vlad.pavel@autodesk.com>
2020-01-06 12:20:59 +0200

    remove AdvanceSteelServices.cs

--- src/AdvanceSteelServices/ElementBinder.cs
+++ src/AdvanceSteelServices/ElementBinder.cs
@@ -5,39 +5,6 @@ using System.Runtime.Serialization;
 namespace Dynamo.Applications.AdvanceSteel.Services
 {
   /// <summary>
-  /// Holds a representation of a AutoCAD Handle that supports serialization
-  /// </summary>
-  [Serializable]
-  public class SerializableHandle : ISerializable
-  {
-    public string Handle { get; set; }
-
-    public SerializableHandle()
-    {
-      Handle = "";
-    }
-
-    /// <summary>
-    /// Ctor used by the serialization engine
-    /// </summary>
-    /// <param name="info"></param>
-    /// <param name="context"></param>
-    public SerializableHandle(SerializationInfo info, StreamingContext context)
-    {
-      Handle = (string)info.GetValue("Handle", typeof(string));
-    }
-    /// <summary>
-    /// Populates a System.Runtime.Serialization.SerializationInfo with the data needed
-    /// </summary>
-    /// <param name="info"></param>
-    /// <param name="context"></param>
-    public void GetObjectData(SerializationInfo info, StreamingContext context)
-    {
-      info.AddValue("Handle", Handle, typeof(string));
-    }
-  }
-
-  /// <summary>
   /// Tools to handle the binding and interaction
   /// </summary>
   public class ElementBinder
---
+++ src/AdvanceSteelServices/SerializableHandle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamo.Applications.AdvanceSteel.Services
+{
+  /// <summary>
+  /// Holds a representation of a AutoCAD Handle that supports serialization
+  /// </summary>
+  [Serializable]
+  public class SerializableHandle : ISerializable
+  {
+    public string Handle { get; set; }
+
+    public SerializableHandle()
+    {
+      Handle = "";
+    }
+
+    /// <summary>
+    /// Ctor used by the serialization engine
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="context"></param>
+    public SerializableHandle(SerializationInfo info, StreamingContext context)
+    {
+      Handle = (string)info.GetValue("Handle", typeof(string));
+    }
+    /// <summary>
+    /// Populates a System.Runtime.Serialization.SerializationInfo with the data needed
+    /// </summary>
+    /// <param name="info"></param>
+    /// <param name="context"></param>
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+      info.AddValue("Handle", Handle, typeof(string));
+    }
+  }
+
+}
