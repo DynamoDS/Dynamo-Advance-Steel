0212be0a7e5086fac4923de27ca76282075d0279
Stephan Gumpert <stephan.gumpert@gmail.com>
2020-12-10 20:42:14 +1000

    Added Special Parts Node Added Special Parts to Selection Node Added Property Nodes for Special Parts

---
+++ src/AdvanceSteelNodesUI/NonSteelElements/ASGetPropertiesSpecialParts.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using CoreNodeModels;
+using Dynamo.Graph.Nodes;
+using Dynamo.Utilities;
+using ProtoCore.AST.AssociativeAST;
+using Newtonsoft.Json;
+
+namespace AdvanceSteel.Nodes
+{
+	[NodeName("Special Parts Properties - Get")]
+	[NodeDescription("Select Advance Steel Special Parts Property Type to Get")]
+  [NodeCategory("AdvanceSteel.Nodes.Properties.Properties-Read")]
+  [OutPortNames("Get Special Parts Property")]
+  [OutPortTypes("string")]
+  [OutPortDescriptions("string")]
+  [IsDesignScriptCompatible]
+	public class ASGetPropertiesSpecialParts : AstDropDownBase
+	{
+		private const string outputName = "Advance Steel Special Parts Properties to Get";
+
+    public ASGetPropertiesSpecialParts()
+				: base(outputName)
+		{
+			InPorts.Clear();
+			OutPorts.Clear();
+			RegisterAllPorts();
+		}
+
+		[JsonConstructor]
+		public ASGetPropertiesSpecialParts(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts)
+		: base(outputName, inPorts, outPorts)
+		{
+		}
+
+		protected override SelectionState PopulateItemsCore(string currentSelection)
+		{
+			Items.Clear();
+
+      var newItems = new List<DynamoDropDownItem>() { };
+      foreach (var item in Utils.GetSpecialPartPropertyList(ePropertyDataOperator.Get))
+      {
+        newItems.Add(new DynamoDropDownItem(item.Key, item.Value));
+      }
+			Items.AddRange(newItems);
+
+			SelectedIndex = 0;
+			return SelectionState.Restore;
+		}
+
+		public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+		{
+      if (Items.Count == 0 ||
+          Items[SelectedIndex].Name == "None" ||
+          SelectedIndex < 0)
+      {
+        return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode()) };
+      }
+
+      var intNode = AstFactory.BuildPrimitiveNodeFromObject((string)Items[SelectedIndex].Name);
+      var assign = AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), intNode);
+
+      return new List<AssociativeNode> { assign };
+
+    }
+	}
+}
