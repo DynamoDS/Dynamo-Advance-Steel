65e8d7f77c2b09b24f889d0fd0ffc8c8eea7974f
phliberato <phliberato@gmail.com>
2022-03-14 16:39:03 -0300

    Layer Dropdown

---
+++ src/AdvanceSteelNodes/Util/LayerUtils.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceSteel.Nodes.Util
+{
+  public static class LayerUtils
+  {
+    public static List<LayerTableRecord> GetAllLayers()
+    {
+      Database db = HostApplicationServices.WorkingDatabase;
+      var trans = db.TransactionManager.TopTransaction;
+
+      LayerTable lt = trans.GetObject(db.LayerTableId, OpenMode.ForRead) as LayerTable;
+
+      List<LayerTableRecord> listaLayers = new List<LayerTableRecord>();
+      foreach (ObjectId ltrId in lt)
+      {
+        LayerTableRecord ltr = trans.GetObject(ltrId, OpenMode.ForRead) as LayerTableRecord;
+
+        if (ltr != null && !ltr.ObjectId.IsNull && !ltr.ObjectId.IsErased)
+          listaLayers.Add(ltr);
+      }
+
+      return listaLayers;
+    }
+  }
+}
---
+++ src/AdvanceSteelNodesUI/Miscellaneous/AcadLayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using CoreNodeModels;
+using Dynamo.Graph.Nodes;
+using Dynamo.Utilities;
+using ProtoCore.AST.AssociativeAST;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+using CADObjectId = Autodesk.AutoCAD.DatabaseServices.ObjectId;
+using AdvanceSteel.Nodes.Util;
+
+namespace AdvanceSteel.Nodes
+{
+  [NodeName("GetLayer")]
+  [NodeCategory("AdvanceSteel.Nodes.Util.CAD")]
+  [NodeDescription("Get model layers")]
+  [IsDesignScriptCompatible]
+  public class AcadLayer : AcadDropDownBase
+  {
+    private const string outputName = "layer";
+
+    public AcadLayer() : base(outputName)
+    {
+
+    }
+
+    [JsonConstructor]
+    public AcadLayer(IEnumerable<PortModel> inPorts, IEnumerable<PortModel> outPorts) : base(outputName, inPorts, outPorts)
+    {
+
+    }
+
+    protected override List<(CADObjectId, string)> GetListItems()
+    {
+      return LayerUtils.GetAllLayers().Select(x => (x.ObjectId, x.Name)).ToList();
+    }
+
+    public override IEnumerable<AssociativeNode> BuildOutputAst(List<AssociativeNode> inputAstNodes)
+    {
+      if (!CanBuildOutputAst())
+      {
+        return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildNullNode()) };
+      }
+
+      return new[] { AstFactory.BuildAssignment(GetAstIdentifierForOutputIndex(0), AstFactory.BuildStringNode(Items[SelectedIndex].Name)) };
+    }
+  }
+}
